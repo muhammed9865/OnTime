df907f54e74f410303ec2c91339a5162d25e9e84
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-04-30 00:25:07 +0200

    new: changing destinations Schedules-To-Notes and vice versa

--- app/src/main/java/com/muhammed/ontime/activity/MainActivity.kt
+++ app/src/main/java/com/muhammed/ontime/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.muhammed.ontime.R
 import com.muhammed.ontime.databinding.ActivityMainBinding
 
@@ -12,14 +13,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(LayoutInflater.from(this))
     }
     private val navController: NavController by lazy {
-        NavController(this)
+        Navigation.findNavController(this, binding.mainFragmentsContainer.id)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
 
-        // Handling the Radio group changes listener.
+        // Handling the Destinations Radio group changes listener.
         binding.destRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
             when(radioGroup.checkedRadioButtonId) {
                 binding.scheduleSectionRb.id -> {
